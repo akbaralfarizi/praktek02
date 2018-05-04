32d83199d669f14f2514c6685839c1e180c2b02a
Akhmad Akbar Alfarizi <32434489+akbaralfarizi@users.noreply.github.com>
2018-05-05 04:03:17 +0700

    Mengisi Nilai Atribute

--- praktek02/src/praktek02/sepatuAksi.java
+++ praktek02/src/praktek02/sepatuAksi.java
@@ -2,7 +2,27 @@ package praktek02;
 public class sepatuAksi {
     public static void main(String[] args) {
         sepatu jalan = new sepatu();
+        jalan.merk="Adidas";
+            jalan.warna="Merah";
+            jalan.bahan="Kulit";
+            jalan.size="41";
+            jalan.madein="EUR";
+            jalan.CetakInfo();
+            
         sepatu sekolah = new sepatu();
+         sekolah.merk="Nike";
+            sekolah.warna="Hitam";
+            sekolah.bahan="Kain";
+            sekolah.size="40";
+            sekolah.madein="Vietnam";
+            sekolah.CetakInfo();
+            
         sepatu kerja = new sepatu();
+        kerja.merk="Converse";
+            kerja.warna="Biru";
+            kerja.bahan="Canvas";
+            kerja.size="41";
+            kerja.madein="Indonesia";
+            kerja.CetakInfo();
     }
 }
