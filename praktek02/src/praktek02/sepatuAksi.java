d7f8e1612e0d47e459b40e2960e7021293449e8b
Akhmad Akbar Alfarizi <32434489+akbaralfarizi@users.noreply.github.com>
2018-05-05 03:59:45 +0700

    Instance Object

--- praktek02/src/praktek02/sepatuAksi.java
+++ praktek02/src/praktek02/sepatuAksi.java
@@ -1,6 +1,8 @@
 package praktek02;
 public class sepatuAksi {
     public static void main(String[] args) {
-        
+        sepatu jalan = new sepatu();
+        sepatu sekolah = new sepatu();
+        sepatu kerja = new sepatu();
     }
 }
